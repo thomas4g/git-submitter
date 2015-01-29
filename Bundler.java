214a529c99bfb88747bcecbaa4dcdef83edba0c0
Thomas Shields <thomas.shields@gatech.edu>
2015-01-29 05:12:51 -0500

    gitignore updates and fixing checkstyle

--- Bundler.java
+++ Bundler.java
@@ -8,11 +8,8 @@ import javax.crypto.spec.SecretKeySpec;
 
 import java.security.spec.X509EncodedKeySpec;
 import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.InvalidKeySpecException;
 
 import java.security.KeyFactory;
-import java.security.PublicKey;
-import java.security.PrivateKey;
 import java.security.NoSuchAlgorithmException;
 import java.security.InvalidKeyException;
 
@@ -29,27 +26,27 @@ import java.util.zip.ZipEntry;
 import java.util.Arrays;
 
 public class Bundler {
-    private static PrintStream DEBUG = System.out;
-    private final static int AES_KEY_SIZE = 128; 
+    private static final PrintStream DEBUG = System.out;
+    private static final int AES_KEY_SIZE = 128;
 
-    private static byte[] readFile(String s) throws IOException { 
+    private static byte[] readFile(String s) throws IOException {
         return readFile(new File(s));
     }
     private static byte[] readFile(File f) throws IOException {
-        byte[] bytes = new byte[(int)f.length()];
+        byte[] bytes = new byte[(int) f.length()];
         new DataInputStream(new FileInputStream(f)).readFully(bytes);
         return bytes;
     }
 
-   private static void saveAESKey(byte[] aesKey, File out, String pubKey) 
+    private static void saveAESKey(byte[] aesKey, File out, String pubKey)
         throws IOException {
         Cipher rsaCipher = null;
-        
+
         try {
             rsaCipher = Cipher.getInstance("RSA");
             rsaCipher.init(Cipher.ENCRYPT_MODE, KeyFactory.getInstance("RSA")
                 .generatePublic(new X509EncodedKeySpec(readFile(pubKey))));
-        } catch(Exception ex) {
+        } catch (Exception ex) {
             throw new IOException("Something wrong with the public keyfile");
         }
 
@@ -62,17 +59,18 @@ public class Bundler {
     private static byte[] loadAESKey(File file, String privateKey)
         throws IOException {
         Cipher rsaCipher = null;
-        
+
         try {
             rsaCipher = Cipher.getInstance("RSA");
             rsaCipher.init(Cipher.DECRYPT_MODE, KeyFactory.getInstance("RSA")
-                .generatePrivate(new PKCS8EncodedKeySpec(readFile(privateKey))));
-        } catch(Exception ex) {
+                .generatePrivate(
+                 new PKCS8EncodedKeySpec(readFile(privateKey))));
+        } catch (Exception ex) {
             throw new IOException("Something wrong with the private keyfile");
         }
 
         byte[] outBytes = new byte[AES_KEY_SIZE / 8];
-        
+
         CipherInputStream cos = new CipherInputStream(
             new FileInputStream(file), rsaCipher);
         cos.read(outBytes);
@@ -82,23 +80,23 @@ public class Bundler {
 
     private static void encrypt(File file, File out, File aesOut,
         String pubKey) throws IOException, InvalidKeyException {
-       
+
         Cipher aesCipher = null;
         try {
             aesCipher = Cipher.getInstance("AES");
             KeyGenerator gen = KeyGenerator.getInstance("AES");
             gen.init(AES_KEY_SIZE);
-            byte[] aesKey = gen.generateKey().getEncoded(); 
+            byte[] aesKey = gen.generateKey().getEncoded();
 
             DEBUG.println("Saving AES Key");
             saveAESKey(aesKey, aesOut, pubKey);
             DEBUG.println("Creating Spec for AES Key");
             SecretKeySpec aesKeySpec = new SecretKeySpec(aesKey, "AES");
             DEBUG.println("Initializing Cipher");
-            aesCipher.init(Cipher.ENCRYPT_MODE, 
+            aesCipher.init(Cipher.ENCRYPT_MODE,
                 aesKeySpec);
-        } catch(NoSuchAlgorithmException | NoSuchPaddingException | 
-            InvalidKeyException ex ) {
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException
+            | InvalidKeyException ex) {
             throw new InvalidKeyException("Problem creating the AES key", ex);
         }
 
@@ -110,39 +108,40 @@ public class Bundler {
 
     private static void decrypt(File file, File out, File aesIn,
         String privateKey) throws IOException {
-       
+
         Cipher aesCipher = null;
         try {
             aesCipher = Cipher.getInstance("AES");
-            
-            DEBUG.println("Loading AES Key");
-            byte[] aesKey = loadAESKey(aesIn, privateKey); 
 
-            aesCipher.init(Cipher.DECRYPT_MODE, 
+            DEBUG.println("Loading AES Key");
+            byte[] aesKey = loadAESKey(aesIn, privateKey);
+
+            aesCipher.init(Cipher.DECRYPT_MODE,
                 new SecretKeySpec(aesKey, "AES"));
-        } catch(NoSuchAlgorithmException | NoSuchPaddingException | 
-            InvalidKeyException ex ) {
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException
+            | InvalidKeyException ex) {
             throw new IOException("Problem loading the AES key");
         }
 
         CipherInputStream cos = new CipherInputStream(
             new FileInputStream(file), aesCipher);
-        
+
         int c;
         FileOutputStream fos = new FileOutputStream(out);
-        while((c = cos.read()) != -1) {
+        while ((c = cos.read()) != -1) {
             fos.write(c);
         }
         fos.close();
         cos.close();
     }
- 
 
-    private static File zipFiles(String name, File... files) throws IOException {
+
+    private static File zipFiles(String name, File... files)
+        throws IOException {
         File zip = new File(name);
         ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zip));
 
-        for(File f : files) {
+        for (File f : files) {
             out.putNextEntry(new ZipEntry(f.getName()));
             byte[] data = readFile(f);
             out.write(data, 0, data.length);
@@ -153,8 +152,9 @@ public class Bundler {
         return zip;
     }
 
-    public static void main(String... args) throws IOException, InvalidKeyException {
-        if(args[0].equals("-d")) {
+    public static void main(String... args)
+        throws IOException, InvalidKeyException {
+        if (args[0].equals("-d")) {
             decrypt(new File(args[1]), new File("decrypted-" + args[1]),
                 new File(args[2]), "private_key.der");
         } else {
