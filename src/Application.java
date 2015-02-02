4e2b1e8e9bf6b870c4f258a7cbdec77e1859b63c
Thomas Shields <thomas.shields@gatech.edu>
2015-02-02 02:25:06 -0500

    Restructured, added json config feature, better ant file
    Apologies for the meager commit logs. Updates here include
    a better program structure w/ fancy smancy ant file, a sample
    json file for the json config feature, and some cosmetics

--- src/Application.java
+++ src/Application.java
@@ -1,11 +1,17 @@
 import java.util.List;
 import java.util.Arrays;
 import java.util.Base64;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 import java.io.File;
 
+import com.google.gson.Gson;
+
 public class Application {
+    
+    private final static String DEFAULT_CONFIG = "config.json";
+    
     private static boolean checkCompile(String[] files) throws Exception {
         List<String> args = Arrays.stream(files).filter(f -> f
             .substring(f.lastIndexOf('.')).equals(".java"))
@@ -45,16 +51,23 @@ public class Application {
         return new StudentSubmission(user, base64, repo);
     }
 
+    private static Config processConfig(File f) throws Exception {
+         return new Gson().fromJson(new Scanner(f).useDelimiter("\\A").next(), Config.class);
+    }
+
+    private static class Config {
+        public String submissionName;
+        public String[] collaborators;
+        public String commitMsg;
+        public String repo;
+    }
     public static void main(String... args) throws Exception {
-        /* Input Vars, will come from JSON eventually, hard-coded for now */
-        String submissionName = "submission.zip";
-        String[] collaborators = {"tshields3"};
-        String commitMsg = "Autosubmmited at <time> by the 1331 Submission JAR";
-        String repo = "cs1331-timed-lab-1-31-15";
+
+        Config config = processConfig(new File(DEFAULT_CONFIG));
 
         System.out.print("Compiling files.....");
         if (!checkCompile(args)) {
-            System.out.println("Your submission does not compile. "
+            System.out.println("\nYour submission does not compile. "
                 + "Please fix your compile errors before proceeding.");
             System.exit(1);
         }
@@ -65,14 +78,14 @@ public class Application {
 
         String[] fileNames = args; //change
         System.out.print("Zipping files.......");
-        File submissionZip = Utils.zipFiles(submissionName,
+        File submissionZip = Utils.zipFiles(config.submissionName + ".zip",
             Arrays.stream(fileNames).map(File::new).toArray(File[]::new));
         System.out.println("...done");
 
-        StudentSubmission sub = authenticateAndCreate(repo);
-        for (String s : collaborators) sub.addCollab(s);
+        StudentSubmission sub = authenticateAndCreate(config.repo);
+        for (String s : config.collaborators) sub.addCollab(s);
 
-        if (sub.pushFile(submissionZip, commitMsg))
+        if (sub.pushFile(submissionZip, config.commitMsg))
             System.out.println("Code submitted successfully!");
         else
             System.out.println("Submission unsuccessful. :(");
