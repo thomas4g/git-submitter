181c7df9322b84fac58a012dc7d46ea37406c895
Keith Cartledge <kjcartledge@users.noreply.github.com>
2015-09-22 21:41:44 -0400

    Moved updating the message to a helper method

--- src/main/java/org/cs1331/gitsubmitter/StudentSubmission.java
+++ src/main/java/org/cs1331/gitsubmitter/StudentSubmission.java
@@ -182,6 +182,17 @@ public class StudentSubmission {
         return code >= 200 && code < 300;
     }
 
+    /**
+     * Appends the identifer of the submitting computer to the body
+     * of the given commit message.
+     *
+     * @param message the commit message to append to
+     * @return the commit message with the identifer appended
+     */
+    private String appendIdentifier(String message) {
+        return message + "\n\nID: " + Utils.getIdentifier();
+    }
+
     private SHAObject createCommit(Commit commit) throws Exception {
         logger.info("Creating commit...");
         StringBuilder sb = new StringBuilder();
@@ -201,7 +212,7 @@ public class StudentSubmission {
 
     public boolean pushFiles(String message, String ... fileNames)
             throws Exception {
-        message = message + "\n\nID: " + Utils.getIdentifier();
+        message = appendIdentifier(message);
         SHAObject tree = createTree(null, fileNames);
         Ref head = getHeadRef();
         SHAObject newRef = createCommit(new Commit(message, tree.sha,
