e103dd53216a5f47e5cb77b4fbe3937d34a59f7b
Keith Cartledge <kcartledge3@gatech.edu>
2016-01-11 01:22:09 -0500

    CLI organization and updates

--- src/main/java/org/cs1331/gitsubmitter/Application.java
+++ src/main/java/org/cs1331/gitsubmitter/Application.java
@@ -20,89 +20,32 @@ public class Application {
 
     private static final String DEFAULT_CONFIG = "json";
 
-    private static boolean checkCompile(String[] files) throws Exception {
-        String combined = Arrays.stream(files).collect(Collectors.joining(" "));
-        List<String> args = Arrays.stream(combined.split(" ")).filter(f -> f
-            .endsWith(".java")).collect(Collectors.toList());
-        if (args.size() == 0) {
-            return true;
-        }
-        args.add(0, "javac");
-        ProcessBuilder pcb = new ProcessBuilder(args);
-        Process p = pcb.start();
-        p.waitFor();
-        return p.exitValue() == 0;
-    }
+    private static final String FORCE_SUBMIT_OPT = "F";
+    private static final String OVERRIDE_MESSAGE_OPT = "m";
+    private static final String ADD_COLLABS_OPT = "c";
+    private static final String OVERRIDE_FILES_OPT = "f";
+    private static final String SHOW_SUBMISSION_OPT = "s";
+    private static final String REPO_NAME_OPT = "r";
+    private static final String USERNAME_OPT = "u";
+    private static final String PASSWORD_OPT = "p";
 
-    private static final String SUBMISSION_OPT  = "s";
-    private static final String COMPILE_OPT     = "j";
-    private static final String REPO_OPT        = "r";
-    private static final String COMMIT_OPT      = "m";
-    private static final String COLLABS_OPT     = "c";
-    private static final String FILES_OPT       = "f";
+    private static final Options options = createOptions();
 
 
     public static void main(String... args) throws Exception {
-        Options options = new Options();
-        options.addOption(SUBMISSION_OPT, "confirm-submission", false,
-            "Open submission in web browser to confirm successful submission");
-        options.addOption(COMPILE_OPT, "compile", false,
-            "Check the submission compiles before submitting and reject if it does not.");
-        options.addOption(Option.builder(REPO_OPT)
-            .required()
-            .longOpt("repo-name")
-            .hasArg()
-            .argName("repository_name")
-            .desc("Required: The name of the repository")
-            .build());
 
-        options.addOption(Option.builder(COMMIT_OPT)
-            .longOpt("commit-message")
-            .hasArg()
-            .argName("message")
-            .desc("An optional commit message. If none is specified, the current time will be used")
-            .build());
-
-        options.addOption(Option.builder(COLLABS_OPT)
-            .longOpt("collabs")
-            .numberOfArgs(Option.UNLIMITED_VALUES)
-            .argName("user1 user2 user3...")
-            .desc("An optional list of users to add as collaborators (who will have fork "
-                + "privileges")
-            .build());
-
-        options.addOption(Option.builder(FILES_OPT)
-            .longOpt("files")
-            .numberOfArgs(Option.UNLIMITED_VALUES)
-            .argName("file1 file2 file3...")
-            .desc("An optional list of files to submit. If no files are specified, src/main/java "
-                + "will be recursively searched for Java files")
-            .build());
-
-
-        HelpFormatter formatter = new HelpFormatter();
-
-        CommandLineParser parser = new DefaultParser();
-        CommandLine line = null;
-        try {
-            line = parser.parse(options, args);
-        } catch (ParseException exp) {
-            System.err.println("Parsing failed.  Reason: " + exp.getMessage());
-            formatter.printHelp("cmd_to_run", options);
-            System.exit(1);
-        }
-
+        CommandLine line = parseArgs(args);
 
         try {
 
             String[] files = {"src/main/java"};
-            if (line.hasOption(FILES_OPT)) {
-                files = line.getOptionValues(FILES_OPT);
+            if (line.hasOption(OVERRIDE_FILES_OPT)) {
+                files = line.getOptionValues(OVERRIDE_FILES_OPT);
             }
             files = Arrays.stream(files).map(s -> s.replaceAll("'", "")).toArray(String[]::new);
 
-            if (line.hasOption(COMPILE_OPT)) {
-                System.out.print("Compiling files.....");
+            if (!line.hasOption(FORCE_SUBMIT_OPT)) {
+                System.out.print("Compiling files...");
                 if (!checkCompile(files)) {
                     System.out.println("\nYour submission does not compile. "
                         + "Please fix your compile errors before proceeding.");
@@ -111,22 +54,34 @@ public class Application {
                 System.out.println("...done");
             }
 
-            String repoName = line.getOptionValue(REPO_OPT);
+            String repoName = line.getOptionValue(REPO_NAME_OPT);
+
+            // Currently Not Used
+            String username = null;
+            if (line.hasOption(USERNAME_OPT)) {
+                username = line.getOptionValue(USERNAME_OPT);
+            }
+
+            // Currently Not Used
+            char[] password = null;
+            if (line.hasOption(PASSWORD_OPT)) {
+                password = line.getOptionValue(PASSWORD_OPT).toCharArray();
+            }
 
             AuthenticatedUser user = AuthenticatedUser.create();
             StudentSubmission sub = new StudentSubmission(user, repoName);
             sub.createRepo();
 
-            if (line.hasOption(COLLABS_OPT)) {
-                for (String collab : line.getOptionValues(COLLABS_OPT)) {
+            if (line.hasOption(ADD_COLLABS_OPT)) {
+                for (String collab : line.getOptionValues(ADD_COLLABS_OPT)) {
                     System.out.println("Adding: " + collab);
                     sub.addCollab(collab);
                 }
             }
 
             String commitMsg = "Solution progress at " + LocalDateTime.now();
-            if (line.hasOption(COMMIT_OPT)) {
-                commitMsg = line.getOptionValue(COMMIT_OPT);
+            if (line.hasOption(OVERRIDE_MESSAGE_OPT)) {
+                commitMsg = line.getOptionValue(OVERRIDE_MESSAGE_OPT);
             }
 
             boolean success = false;
@@ -137,7 +92,7 @@ public class Application {
             }
             if (success) {
                 System.out.println("Code submitted successfully!");
-                if (line.hasOption(SUBMISSION_OPT)) {
+                if (line.hasOption(SHOW_SUBMISSION_OPT)) {
                     System.out.println("Launching browser to view repo...");
                     Desktop.getDesktop().browse(new URI(
                         String.format("http://github.gatech.edu/%s/%s",
@@ -160,4 +115,111 @@ public class Application {
         }
     }
 
+    private static boolean checkCompile(String[] files) throws Exception {
+        String combined = Arrays.stream(files).collect(Collectors.joining(" "));
+        List<String> args = Arrays.stream(combined.split(" ")).filter(f -> f
+            .endsWith(".java")).collect(Collectors.toList());
+        if (args.size() == 0) {
+            return true;
+        }
+        args.add(0, "javac");
+        ProcessBuilder pcb = new ProcessBuilder(args);
+        Process p = pcb.start();
+        p.waitFor();
+        return p.exitValue() == 0;
+    }
+
+    private static Options createOptions() {
+        Options options = new Options();
+        options.addOption(Option.builder(FORCE_SUBMIT_OPT)
+                .longOpt("force")
+                .hasArg(false)
+                .required(false)
+                .desc("force the submission of files even if they do not "
+                    + "compile")
+                .build());
+        options.addOption(Option.builder(OVERRIDE_MESSAGE_OPT)
+                .longOpt("message")
+                .hasArg(true)
+                .optionalArg(false)
+                .argName("\"message\"")
+                .required(false)
+                .desc("override the default commit message with a custom one")
+                .build());
+        options.addOption(Option.builder(ADD_COLLABS_OPT)
+                .longOpt("collabs")
+                .hasArgs()
+                .optionalArg(false)
+                .argName("user1 user2 user3...")
+                .required(false)
+                .desc("add other users a collaborators to the repo with fork "
+                    + "privedges")
+                .build());
+        options.addOption(Option.builder(OVERRIDE_FILES_OPT)
+                .longOpt("files")
+                .hasArgs()
+                .optionalArg(false)
+                .argName("file1 file2 file3...")
+                .required(false)
+                .desc("submit a list of files instead of all Java source "
+                    + "files found via recursive search (default)")
+                .build());
+        options.addOption(Option.builder(SHOW_SUBMISSION_OPT)
+                .longOpt("show")
+                .hasArg(false)
+                .required(false)
+                .desc("attempt to open the submission in a web browser")
+                .build());
+        options.addOption(Option.builder(REPO_NAME_OPT)
+                .longOpt("repo")
+                .hasArg(true)
+                .optionalArg(false)
+                .argName("repository")
+                .required(false)
+                .desc("the name of the repository to submit files to, if not "
+                    + "provided, this value will be prompted for")
+                .build());
+        options.addOption(Option.builder(USERNAME_OPT)
+                .longOpt("username")
+                .hasArg(true)
+                .optionalArg(false)
+                .argName("username")
+                .required(false)
+                .desc("the username of the account to submit files to, if not "
+                    + "provided, this value will be prompted for")
+                .build());
+        options.addOption(Option.builder(PASSWORD_OPT)
+                .longOpt("password")
+                .hasArg(true)
+                .optionalArg(false)
+                .argName("password")
+                .required(false)
+                .desc("the password of the account to submit files to, if not "
+                    + "provided, this value will be prompted for")
+                .build());
+        return options;
+    }
+
+    private static void printHelp() {
+        final String header = "Submit files to a Github repository. Will "
+            + "submit all Java source files found via recursive search as "
+            + "long as they compile with a timestamp as the commit message "
+            + "if none of -FmcfC are specified.\n\n";
+        final String footer = "\nPlease report issues at http://github.com/"
+            + "thomas4j/git-submitter";
+        final HelpFormatter help = new HelpFormatter();
+        help.printHelp("java -jar git-submitter", header, options, footer, true);
+    }
+
+    private static CommandLine parseArgs(String[] args) {
+        CommandLineParser parser = new DefaultParser();
+        CommandLine line = null;
+        try {
+             line = parser.parse(options, args);
+        } catch (ParseException e) {
+            printHelp();
+            System.exit(1);
+        }
+        return line;
+    }
 }
